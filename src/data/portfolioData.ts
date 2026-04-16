06d7113b52c29c02d54491406ec9039169f7e084
sanathmandala391-stack <sanathmandala391@gmail.com>
2026-04-16 20:54:03 +0530

    Fix image URL formatting in portfolioData.ts

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -85,7 +85,7 @@ export const portfolioData: PortfolioData = {
       id: '7',
       title: 'E-Commerce Admin-Dashboard',
       description: 'An e-commerce website is an online platform that allows users to browse, buy, and sell products or services over the internet.',
-      image: 'https://im{ages.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=800',
+      image: 'https://images.pexels.com/photos/230544/pexels-photo-230544.jpeg?auto=compress&cs=tinysrgb&w=800',
       technologies: ['React', 'Node.js', 'Express.js','MongoDb', 'Cloudinary'],
       liveUrl: 'https://admin-dashboard-coral-five-81.vercel.app/',
       githubUrl: 'https://github.com/sanathmandala391-stack/',
